f6ef49fdb76e3a00c3c5e9977ba02e650ff055d3
wydwww <wydwww@gmail.com>
2023-01-04 17:39:13 +0800

    Add and test Residual_block

--- SSSD_TF/imputers/SSSDS4Imputer.py
+++ SSSD_TF/imputers/SSSDS4Imputer.py
@@ -25,3 +25,67 @@ class ZeroConv1d(tf.keras.layers.Layer):
     def call(self, x):
         out = self.conv(x)
         return out
+
+class Residual_block(tf.keras.Model):
+    def __init__(self, res_channels, skip_channels, 
+                 diffusion_step_embed_dim_out, in_channels,
+                s4_lmax,
+                s4_d_state,
+                s4_dropout,
+                s4_bidirectional,
+                s4_layernorm):
+        super(Residual_block, self).__init__()
+        self.res_channels = res_channels
+
+
+        self.fc_t = tf.keras.layers.Dense(units=self.res_channels)
+        self.S41 = S4Layer(features=2*self.res_channels, 
+                          lmax=s4_lmax,
+                          N=s4_d_state,
+                          dropout=s4_dropout,
+                          bidirectional=s4_bidirectional,
+                          layer_norm=s4_layernorm)
+ 
+        self.conv_layer = Conv(self.res_channels, 2 * self.res_channels, kernel_size=3)
+
+        self.S42 = S4Layer(features=2*self.res_channels, 
+                          lmax=s4_lmax,
+                          N=s4_d_state,
+                          dropout=s4_dropout,
+                          bidirectional=s4_bidirectional,
+                          layer_norm=s4_layernorm)
+        
+        self.cond_conv = Conv(2*in_channels, 2*self.res_channels, kernel_size=1)  
+
+        self.res_conv = tf.keras.layers.Conv1D(filters=res_channels, kernel_size=1, data_format="channels_first", kernel_initializer="he_normal", bias_initializer="he_normal")
+        # yiding: Skip weight normalization because it requires activation.
+        
+        self.skip_conv = tf.keras.layers.Conv1D(filters=skip_channels, kernel_size=1, data_format="channels_first", kernel_initializer="he_normal", bias_initializer="he_normal")
+        # yiding: Skip weight normalization because it requires activation.
+
+    def call(self, input_data):
+        x, cond, diffusion_step_embed = input_data
+        h = x
+        B, C, L = x.shape
+        assert C == self.res_channels                      
+                 
+        part_t = self.fc_t(diffusion_step_embed)
+        part_t = tf.reshape(part_t, [B, self.res_channels, 1])  
+        h = h + part_t
+        
+        h = self.conv_layer(h)
+        h = tf.transpose(self.S41(tf.transpose(h, [2, 0, 1])), [1, 2, 0])
+        
+        assert cond is not None
+        cond = self.cond_conv(cond)
+        h += cond
+        
+        h = tf.transpose(self.S42(tf.transpose(h, [2, 0, 1])), [1, 2, 0])
+        
+        out = tf.keras.activations.tanh(h[:,:self.res_channels,:]) * tf.keras.activations.sigmoid(h[:,self.res_channels:,:])
+
+        res = self.res_conv(out)
+        assert x.shape == res.shape
+        skip = self.skip_conv(out)
+
+        return (x + res) * math.sqrt(0.5), skip  # normalize for training stability
--- SSSD_TF/tests/testSSSDS4Imputer.py
+++ SSSD_TF/tests/testSSSDS4Imputer.py
@@ -2,9 +2,9 @@ import sys
 sys.path.insert(1, '/data/3glusterfs/home/yiding/sssd')
 
 from SSSD.src.imputers.SSSDS4Imputer import Conv as TorchConv
-from SSSD_TF.imputers.SSSDS4Imputer import Conv, ZeroConv1d
-
 from SSSD.src.imputers.SSSDS4Imputer import ZeroConv1d as TorchZeroConv1d
+from SSSD.src.imputers.SSSDS4Imputer import Residual_block as TorchResidual_block
+from SSSD_TF.imputers.SSSDS4Imputer import Conv, ZeroConv1d, Residual_block
 
 import torch
 import torch.nn as nn
@@ -55,3 +55,59 @@ if __name__ == '__main__':
     # print(output_pt)
     # print(output_tf)
     # Both are all zeros.
+
+print("Testing Residual_block:")
+
+# Initialize models
+res_channels=256
+skip_channels=256
+diffusion_step_embed_dim_out=512
+in_channels=14
+s4_lmax=100
+s4_d_state=64
+s4_dropout=0.0
+s4_bidirectional=1
+s4_layernorm=1
+
+model_pt = TorchResidual_block(res_channels, skip_channels, 
+                            diffusion_step_embed_dim_out=diffusion_step_embed_dim_out,
+                            in_channels=in_channels,
+                            s4_lmax=s4_lmax,
+                            s4_d_state=s4_d_state,
+                            s4_dropout=s4_dropout,
+                            s4_bidirectional=s4_bidirectional,
+                            s4_layernorm=s4_layernorm)
+model_pt.eval()
+
+model_tf = Residual_block(res_channels, skip_channels, 
+                            diffusion_step_embed_dim_out=diffusion_step_embed_dim_out,
+                            in_channels=in_channels,
+                            s4_lmax=s4_lmax,
+                            s4_d_state=s4_d_state,
+                            s4_dropout=s4_dropout,
+                            s4_bidirectional=s4_bidirectional,
+                            s4_layernorm=s4_layernorm)
+model_tf.trainable = False
+
+# h.shape=torch.Size([500, 256, 100]), conditional.shape=torch.Size([500, 28, 100]), diffusion_step_embed.shape=torch.Size([500, 512])
+# Initialize inputs
+
+h_np = np.random.uniform(0, 1, (500, 256, 100))
+h_pt = torch.Tensor(h_np)
+h_tf = tf.convert_to_tensor(h_np)
+
+conditional_np = np.random.uniform(0, 1, (500, 28, 100))
+conditional_pt = torch.Tensor(conditional_np)
+conditional_tf = tf.convert_to_tensor(conditional_np)
+
+diffusion_step_embed_np = np.random.uniform(0, 1, (500, 512))
+diffusion_step_embed_pt = torch.Tensor(diffusion_step_embed_np)
+diffusion_step_embed_tf = tf.convert_to_tensor(diffusion_step_embed_np)
+
+output_h_pt, skip_n_pt = model_pt((h_pt, conditional_pt, diffusion_step_embed_pt))
+print(f"{output_h_pt.shape=}")
+print(f"{skip_n_pt.shape=}")
+
+output_h_tf, skip_n_tf = model_tf((h_tf, conditional_tf, diffusion_step_embed_tf))
+print(f"{output_h_tf.shape=}")
+print(f"{skip_n_tf.shape=}")
